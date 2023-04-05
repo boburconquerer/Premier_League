ba618979905f7938559c5cbc3c58066a41aa76b8
ICT <boburabdugoffarov@gmail.com>
2023-04-05 19:59:24 +0500

    asa

--- app/src/main/java/com/boburindustry/premierleague/MainActivity.kt
+++ app/src/main/java/com/boburindustry/premierleague/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.boburindustry.premierleague
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
+    lateinit var timer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        timer = object: CountDownTimer(2000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@MainActivity,MainActivity2::class.java)
+                startActivity(intent)
+                finish()
+            }
+    }.start()
     }
 }
